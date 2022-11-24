e980978bf447bb6871c0da7cae78f6b8fdd7531e
jesmok <jedrzejsmok55@gmail.com>
2022-11-24 23:52:51 +0100

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # nbp-api
+
+## Clone repository
+
+```
+git clone https://github.com/jedrzej-smok/nbp-api.git
+```
+
+## Create virtual environment, activate it and install required packages
+
+```
+cd nbp-api
+python -m venv ./venv
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+```
+
+## Run application
+```
+python src/main.py
+```
+
+## Test code
+```
+pytest
+```
--- tests/test_CombineData.py
+++ tests/test_CombineData.py
@@ -24,8 +24,8 @@ def test_extract_data(exchangerates_data, combine_data):
     assert next(gen) == ['2022-11-24', 'dolar amerykański', 'USD', 4.5101]
 
 def test_store_data(combine_data):
-    combine_data.store_data('xd.csv')
-    with open('xd.csv', newline='',encoding='utf-8') as csvfile:
+    combine_data.store_data('out/test.csv')
+    with open('out/test.csv', newline='',encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         assert ','.join(next(reader)) == 'effectiveDate,currency,code,mid'
         assert ','.join(next(reader)) == '2022-11-24,bat (Tajlandia),THB,0.1261'
@@ -34,7 +34,6 @@ def test_store_data(combine_data):
         assert ','.join(next(reader)) == '2022-11-24,dolar amerykański,USD,4.5101'
 
 def test_file_name_generator(combine_data):
-    LOGGER.info(re.match("^2022-11-24--23-", combine_data.file_name_generator()))
-    assert re.match("^2022-11-24--23-", combine_data.file_name_generator())
+    assert re.match("^out/2022-11-24--23-", combine_data.file_name_generator())
     
     
